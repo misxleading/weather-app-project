8640a5da4c6f1c3f86645e4317bff85791042cfb
Mira <a.miroslawaaa@gmail.com>
2022-08-05 20:49:51 +0300

    Updated JS

--- src/index.js
+++ src/index.js
@@ -4,112 +4,13 @@ function showPosition(position) {
   let longitude = position.coords.longitude;
   let apiKey = "019d56ca36b2672fd8b69d369f52f303";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showCurrentTemperature);
+  axios.get(apiUrl).then(showWeather);
 }
 function getPosition(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-function showCurrentTemperature(response) {
-  celsiusTemperature = response.data.main.temp;
-  let temp = Math.round(celsiusTemperature);
-  let selectedCityTemp = document.querySelector("#temp");
-  selectedCityTemp.innerHTML = `${temp}`;
-
-  let city = document.querySelector("h1");
-  city.innerHTML = response.data.name;
-
-  let currentPositionDescription = document.querySelector(".description");
-  currentPositionDescription.innerHTML = `${response.data.weather[0].main}`;
-
-  let currentDate = document.querySelector(".date");
-  currentDate.innerHTML = formatDate(response.data.dt * 1000);
-
-  let highestTemp = Math.round(response.data.main.temp_max);
-  let highElement = document.querySelector("#high");
-  highElement.innerHTML = `${highestTemp}°C`;
-
-  let lowestTemp = Math.round(response.data.main.temp_min);
-  let lowElement = document.querySelector("#low");
-  lowElement.innerHTML = `${lowestTemp}°C`;
-
-  let wind = Math.round(response.data.wind.speed);
-  let windElement = document.querySelector("#wind");
-  windElement.innerHTML = `${wind}km/h`;
-
-  let rain = Math.round(response.data.main.humidity);
-  let rainElement = document.querySelector("#rain");
-  rainElement.innerHTML = `${rain}%`;
-
-  let sunriseElement = document.querySelector("#sunrise");
-  sunriseElement.innerHTML = new Date(
-    response.data.sys.sunrise * 1000
-  ).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-
-  let sunsetElement = document.querySelector("#sunset");
-  sunsetElement.innerHTML = new Date(
-    response.data.sys.sunset * 1000
-  ).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-
-  let iconElement = document.querySelector(".icon");
-  let iconText = response.data.weather[0].icon;
-
-  if (iconText === "01d" || iconText === "01n") {
-    return iconElement.setAttribute(
-      "src",
-      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/325/sun_2600-fe0f.png"
-    );
-  }
-  if (iconText === "02d" || iconText === "02n") {
-    return iconElement.setAttribute(
-      "src",
-      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/325/sun-behind-cloud_26c5.png"
-    );
-  }
-  if (iconText === "03d" || iconText === "03n") {
-    return iconElement.setAttribute(
-      "src",
-      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/325/sun-behind-large-cloud_1f325-fe0f.png"
-    );
-  }
-  if (iconText === "04d" || iconText === "04n") {
-    return iconElement.setAttribute(
-      "src",
-      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/325/cloud_2601-fe0f.png"
-    );
-  }
-  if (iconText === "09d" || iconText === "09n") {
-    return iconElement.setAttribute(
-      "src",
-      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/325/sun-behind-rain-cloud_1f326-fe0f.png"
-    );
-  }
-  if (iconText === "10d" || iconText === "10n") {
-    return iconElement.setAttribute(
-      "src",
-      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/325/cloud-with-rain_1f327-fe0f.png"
-    );
-  }
-  if (iconText === "11d" || iconText === "11n") {
-    return iconElement.setAttribute(
-      "src",
-      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/325/cloud-with-lightning-and-rain_26c8-fe0f.png"
-    );
-  }
-  if (iconText === "13d" || iconText === "13n") {
-    return iconElement.setAttribute(
-      "src",
-      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/325/cloud-with-snow_1f328-fe0f.png"
-    );
-  }
-  if (iconText === "50d" || iconText === "50n") {
-    return iconElement.setAttribute(
-      "src",
-      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/325/fog_1f32b-fe0f.png"
-    );
-  }
-}
 //new date fuction
 function formatDate(timestamp) {
   let now = new Date();
@@ -153,8 +54,17 @@ function formatDate(timestamp) {
   return `Updated: ${currentDay} ${date}, ${currentMonth}, ${hour}:${minute}`;
 }
 
+// function for getting api forecast
+function getForecastApi(coordinates) {
+  console.log(coordinates);
+  let apiKey = "26b44c1b74cfa0b6ee2a57177c15f70e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 // function for forecast
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -164,29 +74,26 @@ function displayForecast() {
     forecastHTML =
       forecastHTML +
       `
-
-  <div class="card">
+      <div class="card">
     <p class="card-date">${day} 00</p>
     <img
       class="card-img-top icons"
       src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/325/cloud-with-lightning-and-rain_26c8-fe0f.png"
-      alt="Card image cap"
+      alt=""
     />
     <div class="card-body">
       <h5 class="card-temp">00°C</h5>
     </div>
   </div>
-
-`;
+  `;
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
 // typed city weather( heading - temp - description)
-function showWeatherForTypedCity(response) {
-  console.log(response);
-  console.log(response.data.name);
+function showWeather(response) {
   let city = document.querySelector("h1");
   city.innerHTML = response.data.name;
 
@@ -229,6 +136,7 @@ function showWeatherForTypedCity(response) {
 
   let iconElement = document.querySelector(".icon");
   let iconText = response.data.weather[0].icon;
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
   if (iconText === "01d" || iconText === "01n") {
     return iconElement.setAttribute(
@@ -284,6 +192,8 @@ function showWeatherForTypedCity(response) {
       "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/325/fog_1f32b-fe0f.png"
     );
   }
+
+  getForecastApi(response.data.coord);
 }
 
 function submitSearching(event) {
@@ -291,7 +201,7 @@ function submitSearching(event) {
   let apiKeyType = "019d56ca36b2672fd8b69d369f52f303";
   let typedCity = document.querySelector("#city-input").value;
   let urlTyped = `https://api.openweathermap.org/data/2.5/weather?q=${typedCity}&appid=${apiKeyType}&units=metric`;
-  axios.get(`${urlTyped}`).then(showWeatherForTypedCity);
+  axios.get(`${urlTyped}`).then(showWeather);
 }
 
 function displayFahrenheitTemperature(event) {
@@ -325,5 +235,3 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let currentButton = document.querySelector("button");
 currentButton.addEventListener("click", getPosition);
-
-displayForecast();
